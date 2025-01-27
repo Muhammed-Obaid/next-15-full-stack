84943e0bf4cf559bf415a8ae26f93c00886d4d3e
Muhammed-Obaid <obaidsultan12@gmail.com>
2025-01-27 13:45:51 +0500

    feat: setup login logout auth

---
+++ app/(root)/page.tsx
@@ -0,0 +1,17 @@
+// 'use client'
+import Image from "next/image";
+// import { useMemo, useRef, useState } from "react";
+
+export default function Home() {
+  // throw new Error('something went wrong');
+  
+
+  return (
+    <section className="pink_container">
+      <h1 className="heading">Pitch your startup <br /> connect with entrepreneurs</h1>
+      <p className="sub-heading !max-w-3xl">
+        Submit ideas, Vote on Pitches, and Get Noticed in Virtual Competitions   
+      </p>
+    </section>
+  );
+}
